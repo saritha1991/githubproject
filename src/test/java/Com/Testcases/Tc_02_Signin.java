26634b8ecb740054704e96925df0eb180364a805
tm <tm@tm6>
2018-07-25 11:59:04 +0530

    committing next

---
+++ src/test/java/Com/Testcases/Tc_02_Signin.java
@@ -0,0 +1,12 @@
+package Com.Testcases;
+
+import org.testng.annotations.Test;
+
+public class Tc_02_Signin {
+	@Test
+	public void signin()
+	{
+		System.out.println("Signin the application successfully");
+	}
+
+}
